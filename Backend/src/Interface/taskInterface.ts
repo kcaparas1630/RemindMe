15ff706ee2aed301da818c7af62825199be9239e
Kent Caparas <kcaparas1630@gmail.com>
2024-11-08 17:06:38 -0800

    basic structure of task post method

--- Backend/src/Interface/taskInterface.ts
+++ Backend/src/Interface/taskInterface.ts
@@ -2,7 +2,7 @@ interface TaskInterface {
     task_name: string;
     task_descrption: string;
     task_progress: string;
-    task_completion_date: Date;
+    task_completion_date?: Date;
 };
 
 export default TaskInterface;
--- Backend/src/controllers/TaskController.ts
+++ Backend/src/controllers/TaskController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import TaskInterface from '../Interface/taskInterface';
 
 const getTask = (req: Request, res: Response) => {
     res.status(200).send('Hello World');
@@ -7,13 +8,22 @@ const getTask = (req: Request, res: Response) => {
 const postTask = (req: Request, res: Response) => {
     const { task_name, task_description, task_progress } = req.body;
 
-    if (task_name || task_description ||task_progress == undefined) {
+    if (task_name || task_description || task_progress == undefined) {
         res.status(400).json({
             success: false,
             message: 'Missing required fields',
         });
     }
 
-    const newTask = 
+    const newTask: TaskInterface = {
+        task_name: task_name,
+        task_descrption: task_description,
+        task_progress: task_progress,
+    };
+
+    res.status(201).json({
+        success: true,
+        message: 'Task has successfully been added',
+    });
 };
 export default getTask;
