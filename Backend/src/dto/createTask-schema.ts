554fbe93f9628b461694cce27ba79e902c889eb9
kcaparas1630 <kcaparas1630@gmail.com>
2024-11-11 11:26:58 -0800

    setup yup to do validations

---
+++ Backend/src/dto/createTask-schema.ts
@@ -0,0 +1,11 @@
+import Yup, { date, object, string } from "yup";
+
+const taskValidationSchema = object({
+    body: object({
+        task_name: string().required('Task name is required'),
+        task_description: string().required('Task Description is required'),
+        task_progress: string().oneOf(['NOTSTARTED', 'STARTED', 'COMPLETED']).required('Task Progres is required'),
+    })
+});
+
+export default taskValidationSchema;
---
+++ Backend/src/middleware/validation-schema.ts
@@ -0,0 +1,21 @@
+import { AnySchema } from "yup";
+import { Request, Response, NextFunction } from "express";
+
+const validate = (schema: AnySchema) => async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        await schema.validate({
+            body: req.body,
+            query: req.query,
+            params: req.params,
+        });
+        next();
+    } catch (e:any) {
+        res.status(400).send(e.message);
+    }
+}
+
+export default validate;
