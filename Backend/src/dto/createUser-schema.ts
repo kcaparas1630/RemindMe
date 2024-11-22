5841a534b1bcadd8f89daafc652f00992a6485cd
Kent Caparas <kcaparas1630@gmail.com>
2024-11-21 21:09:50 -0800

    updated userValidationSchema to add username, password, and email

--- Backend/src/dto/createUser-schema.ts
+++ Backend/src/dto/createUser-schema.ts
@@ -4,7 +4,13 @@ import { object, string } from "yup";
 const userValidationSchema = object({
     body: object({
         first_name: string().required('First name is required'),
-        last_name: string().required('Last name is required')
+        last_name: string().required('Last name is required'),
+        username: string().required('Username is required'),
+        user_password: string().min(8).max(64).matches(
+            /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};:'",.<>\?])/,
+            'Password must contain at least one Uppercase letter, one Lowercase letter, one Number, and one Special character'
+        ).required('Password is required'),
+        user_email: string().email('Email must be a valid email').required('Email is required')
     })
 });
 
