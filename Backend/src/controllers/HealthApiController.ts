ddfaf5d401ed199ddd73aef059844a73976a9c07
kcaparas1630 <kcaparas1630@gmail.com>
2025-01-18 10:58:25 -0800

    removed logging in api

--- Backend/src/controllers/HealthApiController.ts
+++ Backend/src/controllers/HealthApiController.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from 'express';
 
 const checkHealth = (req: Request, res: Response): void => {
-    console.log('Health check endpoint hit'); // Add logging
     try {
         res.status(200).send('Task API is healthy');
-        console.log('Health check responded successfully'); // Add logging
     } catch (error) {
         console.error('Health check error:', error);
         res.status(500).send('Internal server error');
