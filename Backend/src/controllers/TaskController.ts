370219b8eeb3d22206504a19c7a99a09c26d3e68
kcaparas1630 <kcaparas1630@gmail.com>
2025-02-05 20:22:57 -0800

    error handling finished

--- Backend/src/controllers/TaskController.ts
+++ Backend/src/controllers/TaskController.ts
@@ -5,6 +5,7 @@ import DatabaseError from '../ErrorHandlers/DatabaseError';
 import ErrorLogger from '../Helper/LoggerFunc';
 import ValidationError from '../ErrorHandlers/ValidationError';
 import Task from '../Interface/taskInterface';
+import checkTaskNameExists from '../Helper/TaskNameExists';
 
 // get method by task
 const getAllTask = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -25,26 +26,37 @@ const getAllTask = async (req: Request, res: Response, next: NextFunction): Prom
 // Create a task
 const createTask = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
-    const { taskName, taskDescription, taskProgress, taskDueDate }: {
-        taskName: Task['taskName'],
-        taskDescription: Task['taskDescription'],
-        taskProgress: Task['taskProgress'],
-        taskDueDate: Task['taskDueDate']
+    const {
+      taskName,
+      taskDescription,
+      taskProgress,
+      taskDueDate,
+    }: {
+      taskName: Task['taskName'];
+      taskDescription: Task['taskDescription'];
+      taskProgress: Task['taskProgress'];
+      taskDueDate: Task['taskDueDate'];
     } = req.body;
+
+    const taskNameExists = await checkTaskNameExists(taskName);
+
+    
+    if(taskNameExists) {
+        throw new ValidationError('Task Name Already Exists.');
+    }
     const requestBody = await DatabaseService.addTask(
       taskName,
       taskDescription,
       taskProgress,
       new Date(taskDueDate)
-    ); 
-    // TODO: CREATE A FORBIDDEN ROUTE FOR MULTIPLE TASKNAME
+    );
+   
     logger.info('Task has been successfully been added');
     res.status(201).json({
       success: true,
       message: 'Task has successfully been added',
       data: requestBody,
     });
-
   } catch (error: unknown) {
     // DRY ERROR LOGGER
     ErrorLogger(error, 'createTask');
@@ -53,18 +65,22 @@ const createTask = async (req: Request, res: Response, next: NextFunction): Prom
   }
 };
 
-const getTaskByTaskName = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+const getTaskByTaskName = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const { taskName } = req.body;
 
     // throw error if missing task name
     if (!taskName) {
-        throw new ValidationError('Task Name is required');
+      throw new ValidationError('Task Name is required');
     }
     const requestBody = await DatabaseService.getTaskByTaskName(taskName);
 
     if (!requestBody) {
-        throw new DatabaseError('Task not found');
+      throw new DatabaseError('Task not found');
     }
     res.status(200).send(requestBody);
   } catch (error) {
