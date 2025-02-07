1bbce2bf4abb2e560c26883e440ae1d331eafe21
Kent Caparas <kcaparas1630@gmail.com>
2025-02-06 18:20:34 -0800

    added range validation for date

--- Backend/src/dto/createTask-schema.ts
+++ Backend/src/dto/createTask-schema.ts
@@ -1,5 +1,8 @@
 import { date, object, string } from 'yup';
 
+const todayDate: Date = new Date(Date.now());
+const maxDate: Date = new Date('2050-12-31');
+
 const taskValidationSchema = object({
   body: object({
     taskName: string().required('Task name is required'),
@@ -7,7 +10,10 @@ const taskValidationSchema = object({
     taskProgress: string()
       .oneOf(['NOTSTARTED', 'STARTED', 'COMPLETED'])
       .required('Task Progress is required'),
-    taskDueDate: date().required('Due date is required'),
+    taskDueDate: date()
+    .min(todayDate, 'Date must be today or later.')
+    .max(maxDate, 'Date must be no more than December 31, 2050')
+    .required('Due date is required'),
   }),
 });
 
