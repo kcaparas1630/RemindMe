0e7693c972c07bf6e8a21ad1291557a472faae0b
kcaparas1630 <kcaparas1630@gmail.com>
2025-02-11 19:48:21 -0800

    added formState isSubmitting for login to give better user experience

--- Frontend/src/Views/Auth/Components/Login.tsx
+++ Frontend/src/Views/Auth/Components/Login.tsx
@@ -5,8 +5,8 @@
  * @author @Kcaparas
  */
 
-import { FC, useState } from 'react';
-import axios from 'axios';
+import { FC, } from 'react';
+// import axios from 'axios';
 import validationSchema from '../Schema/LoginSchema';
 import { ThemeProvider } from '@emotion/react';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -28,56 +28,14 @@ const Login: FC<GeneralProps> = ({ isDarkMode, toggleTheme }) => {
   const methods = useForm<LoginFormProps>({
     resolver: yupResolver(validationSchema),
   });
-
-  const onSubmit: SubmitHandler<LoginFormProps> = (data) => {
+  // Will update in the next PR
+  const onSubmit: SubmitHandler<LoginFormProps> = async (data) => {
+    await new Promise(resolve => {
+      setTimeout(resolve, 3000) 
+    });
     // eslint-disable-next-line no-console
     console.log(data);
   };
-  /**
-   *
-   * @param values - gets the values from input fields under formik.
-   * @param setSubmitting - a state that returns to true when the user has clicked the submit button/ handleLogin function was called and validation suceeded
-   * @param setErrors - a state that stores the errors and then displays unto the rendered ErrorMessage. Refer to line 113 and other <ErrorMessage /> calls
-   *
-   * @author @Kcaparas
-   */
-  // const handleLogin = async (
-  //   values: LoginFormProps,
-
-  //   {
-  //     setSubmitting,
-  //     setErrors,
-  //   }: // eslint-disable-next-line no-unused-vars
-  //   { setSubmitting: (isSubmitting: boolean) => void; setErrors: (errors: FormikErrors<LoginFormProps>) => void }
-  // ): Promise<void> => {
-  //   try {
-  //     setErrors({});
-  //     setSubmitting(true);
-
-  //     const result = await axios.post('http://localhost:3000/api/user/login', {
-  //       userName: values.userName,
-  //       userPassword: values.userPassword,
-  //     });
-
-  //     localStorage.setItem('loginToken', result.data.token);
-  //     // force a page reload to update authentication state
-  //     window.location.href = '/dashboard';
-  //   } catch (error) {
-  //     if (axios.isAxiosError(error)) {
-  //       const serverError = error.response?.data?.message;
-  //       setErrors(serverError || 'Login failed');
-
-  //       if (error.response?.data?.errors) {
-  //         setErrors(error.response.data.errors);
-  //       }
-  //     } else {
-  //       // requires an object from LoginProps to work. Either field will work.
-  //       setErrors({ userName: 'An unexpected error occurred' });
-  //     }
-  //   } finally {
-  //     setSubmitting(false);
-  //   }
-  // };
 
   return (
     <>
@@ -116,80 +74,20 @@ const Login: FC<GeneralProps> = ({ isDarkMode, toggleTheme }) => {
                   <ErrorMessage>{methods.formState.errors.userPassword?.message}</ErrorMessage>
                 )}
               </InputWrapper>
+              <ThemeProvider theme={{ isDarkMode: isDarkMode }}>
+                    <RouterText to="/register">No Account yet?</RouterText>
+                  </ThemeProvider>
               <Button
                 type="submit"
                 name="Submit"
-                disabled={false}
+                disabled={methods.formState.isSubmitting}
                 isDarkMode={isDarkMode}
               >
-                Submit
+                {methods.formState.isSubmitting ? 'Loading...' : 'Submit'}
               </Button>
             </StyledForm>
           </FormProvider>
 
-          {/* <Formik
-            initialValues={formData}
-            validationSchema={validationSchema}
-            onSubmit={(values, { setSubmitting, setErrors }) => {
-              handleLogin(values, { setSubmitting, setErrors });
-            }}
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-            }) => {
-              return (
-                <StyledForm onSubmit={handleSubmit}>
-                  <InputWrapper>
-                    <InputField
-                      type="text"
-                      inputName="userName"
-                      labelName="Username"
-                      placeholder="Enter your Username"
-                      value={values.userName}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={errors.userName && touched.userName}
-                    />
-                    {errors.userName && touched.userName && (
-                      <ErrorMessage>{errors.userName}</ErrorMessage>
-                    )}
-                  </InputWrapper>
-                  <InputWrapper>
-                    <InputField
-                      type="password"
-                      inputName="userPassword"
-                      labelName="Password"
-                      placeholder="Enter your Password"
-                      value={values.userPassword}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={errors.userPassword && touched.userPassword}
-                    />
-                    {errors.userPassword && touched.userPassword && (
-                      <ErrorMessage>{errors.userPassword}</ErrorMessage>
-                    )}
-                  </InputWrapper>
-                  <ThemeProvider theme={{ isDarkMode: isDarkMode }}>
-                    <RouterText to="/register">No Account yet?</RouterText>
-                  </ThemeProvider>
-                  <Button
-                    type="submit"
-                    name="Submit"
-                    disabled={isSubmitting}
-                    isDarkMode={isDarkMode}
-                  >
-                    {isSubmitting ? 'Logging in' : 'Submit'}
-                  </Button>
-                </StyledForm>
-              );
-            }}
-          </Formik> */}
         </FormContainer>
       </Container>
     </>
