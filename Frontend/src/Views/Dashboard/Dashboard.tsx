0141475f913680860893a40c340b9662c3c3781e
Kent Caparas <kcaparas1630@gmail.com>
2025-02-13 14:24:20 -0800

    implemented getUser hook

--- Frontend/src/Views/Dashboard/Dashboard.tsx
+++ Frontend/src/Views/Dashboard/Dashboard.tsx
@@ -1,15 +1,14 @@
 import { FC, useState } from 'react';
 import Header from '../../Commons/Headers';
-import UserInterface from '../../Interface/UserInterface';
 import { Table, TableHeader, TableCell } from './Styled-Components/StyledTable';
 import Button from '../../Commons/Button';
 import { useNavigate } from 'react-router-dom';
 import GeneralProps from '../../Interface/General/GeneralProps';
 import TaskFormSection from './TaskFormSection';
-import { useQuery, useQueryClient, UseQueryResult } from '@tanstack/react-query';
-import axios from 'axios';
+import { useQueryClient } from '@tanstack/react-query';
 import LoadingSpinner from '../../Commons/LoadingSpinner';
 import { ToastContainer } from 'react-toastify';
+import GetUser from '../../HelperFunctions/GetUser';
 /**
  * this is going to change still.
  * 
@@ -19,7 +18,14 @@ import { ToastContainer } from 'react-toastify';
  * @returns a ReactNode, renders an html element
  * @author @Kcaparas
  */
+
+// get token from local storage
+const token = localStorage.getItem('loginToken');
+const userName: string = token ? JSON.parse(atob(token.split('.')[1])).sub : null;
+
+
 const Dashboard: FC<GeneralProps> = ({ isDarkMode, toggleTheme }) => {
+  const {users, isPending, isError, error} = GetUser(userName, token);
   const [isLogOutClicked, setIsLogoutClicked] = useState<boolean>(false);
   const navigate = useNavigate();
   const queryClient = useQueryClient();
@@ -28,36 +34,14 @@ const Dashboard: FC<GeneralProps> = ({ isDarkMode, toggleTheme }) => {
     localStorage.removeItem('loginToken');
     navigate('/login');
   };
-  // get token from local storage
-  const token = localStorage.getItem('loginToken');
-  const userName: string = token ? JSON.parse(atob(token.split('.')[1])).sub : null;
-  const getTasks = async (userName: string): Promise<UserInterface> => {
-    const response = await axios.get(`http://localhost:3000/api/user/${userName}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return response.data;
-  };
 
-  const {
-    data: users,
-    isPending,
-    isError,
-    error,
-  }: UseQueryResult<UserInterface, Error> = useQuery({
-    queryKey: ['users', userName],
-    queryFn: () => {
-      return getTasks(userName);
-    },
-    enabled: !!userName,
-  });
+
 
   if (isPending) {
     return <LoadingSpinner isDarkMode={isDarkMode} />;
   }
 
-  if (isError) {
+  if (isError && !!error) {
     return <span>{error.message}</span>;
   }
   return (
