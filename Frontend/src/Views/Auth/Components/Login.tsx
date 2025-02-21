1ea3efd3142737697e9012bdaf8c076a81b84011
kcaparas1630 <kcaparas1630@gmail.com>
2025-02-21 12:04:58 -0800

    Adjusting login view based on assets

--- Frontend/src/Views/Auth/Components/Login.tsx
+++ Frontend/src/Views/Auth/Components/Login.tsx
@@ -10,7 +10,13 @@ import axios from 'axios';
 import validationSchema from '../Schema/LoginSchema';
 import { ThemeProvider } from '@emotion/react';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Container, RouterText } from '../Styled-Components/StyledAuth';
+import {
+  Container,
+  RouterText,
+  BannerContainer,
+  FormHolderContainer,
+  BannerImage,
+} from '../Styled-Components/StyledAuth';
 import {
   FormContainer,
   StyledForm,
@@ -25,6 +31,7 @@ import GeneralProps from '../../../Interface/General/GeneralProps';
 import { SubmitHandler, useForm, FormProvider } from 'react-hook-form';
 import { useMutation } from '@tanstack/react-query';
 import ApiErrorResponse from '../../../Interface/ErrorResponse';
+import ManSittingDown from '../../../../assets/Man Sitting Down.svg';
 
 const loginUser = async (credentials: LoginFormProps) => {
   const result = await axios.post('http://localhost:3000/api/user/login', {
@@ -47,7 +54,7 @@ const Login: FC<GeneralProps> = ({ isDarkMode, toggleTheme }) => {
       // force a page reload to update authentication state
       window.location.href = '/main';
     },
-    onError: (error: Error & {response?: { data: ApiErrorResponse}}) => {
+    onError: (error: Error & { response?: { data: ApiErrorResponse } }) => {
       if (axios.isAxiosError(error) && error.response?.data) {
         methods.setError('root', {
           message: error.response.data.message,
@@ -75,53 +82,60 @@ const Login: FC<GeneralProps> = ({ isDarkMode, toggleTheme }) => {
         toggleTheme={toggleTheme}
       />
       <Container>
-        <FormContainer isDarkMode={isDarkMode}>
-          <h1>Task Dashboard Login</h1>
-          <FormProvider {...methods}>
-            <StyledForm onSubmit={methods.handleSubmit(onSubmit)}>
-              <InputWrapper>
-                <InputField
-                  registerName="userName"
-                  type="text"
-                  inputName="userName"
-                  labelName="Username"
-                  placeholder="Enter your Username"
-                  error={methods.formState.errors.userName}
-                />
-                {methods.formState.errors.userName && (
-                  <ErrorMessage>{methods.formState.errors.userName?.message}</ErrorMessage>
-                )}
-              </InputWrapper>
-              <InputWrapper>
-                <InputField
-                  registerName="userPassword"
-                  type="password"
-                  inputName="userPassword"
-                  labelName="Password"
-                  placeholder="Enter your Password"
-                  error={methods.formState.errors.userPassword}
-                />
-                {methods.formState.errors.userPassword && (
-                  <ErrorMessage>{methods.formState.errors.userPassword?.message}</ErrorMessage>
-                )}
-              </InputWrapper>
-              <ThemeProvider theme={{ isDarkMode: isDarkMode }}>
-                <RouterText to="/register">No Account yet?</RouterText>
-              </ThemeProvider>
-              <Button
-                type="submit"
-                name="Submit"
-                disabled={methods.formState.isSubmitting}
-                isDarkMode={isDarkMode}
-              >
-                {methods.formState.isSubmitting ? 'Loading...' : 'Submit'}
-              </Button>
-              {methods.formState.errors.root && (
+        <ThemeProvider theme={{ isDarkMode: isDarkMode }}>
+          <BannerContainer>
+            <BannerImage src={ManSittingDown}  alt='Man Holding a Coffee'/>
+          </BannerContainer>
+        </ThemeProvider>
+        <FormHolderContainer>
+          <FormContainer isDarkMode={isDarkMode}>
+            <h1>Task Dashboard Login</h1>
+            <FormProvider {...methods}>
+              <StyledForm onSubmit={methods.handleSubmit(onSubmit)}>
+                <InputWrapper>
+                  <InputField
+                    registerName="userName"
+                    type="text"
+                    inputName="userName"
+                    labelName="Username"
+                    placeholder="Enter your Username"
+                    error={methods.formState.errors.userName}
+                  />
+                  {methods.formState.errors.userName && (
+                    <ErrorMessage>{methods.formState.errors.userName?.message}</ErrorMessage>
+                  )}
+                </InputWrapper>
+                <InputWrapper>
+                  <InputField
+                    registerName="userPassword"
+                    type="password"
+                    inputName="userPassword"
+                    labelName="Password"
+                    placeholder="Enter your Password"
+                    error={methods.formState.errors.userPassword}
+                  />
+                  {methods.formState.errors.userPassword && (
+                    <ErrorMessage>{methods.formState.errors.userPassword?.message}</ErrorMessage>
+                  )}
+                </InputWrapper>
+                <ThemeProvider theme={{ isDarkMode: isDarkMode }}>
+                  <RouterText to="/register">Forgot your Password?</RouterText>
+                </ThemeProvider>
+                <Button
+                  type="submit"
+                  name="Submit"
+                  disabled={methods.formState.isSubmitting}
+                  isDarkMode={isDarkMode}
+                >
+                  {methods.formState.isSubmitting ? 'Loading...' : 'Submit'}
+                </Button>
+                {methods.formState.errors.root && (
                   <ErrorMessage>{methods.formState.errors.root?.message}</ErrorMessage>
                 )}
-            </StyledForm>
-          </FormProvider>
-        </FormContainer>
+              </StyledForm>
+            </FormProvider>
+          </FormContainer>
+        </FormHolderContainer>
       </Container>
     </>
   );
--- Frontend/src/Views/Auth/Styled-Components/StyledAuth.ts
+++ Frontend/src/Views/Auth/Styled-Components/StyledAuth.ts
@@ -1,30 +1,82 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
-
 const Container = styled.section`
   label: ContainerWrapper;
   display: flex;
   width: 100%;
-  height: 90vh;
+  height: 95vh;
+  position: relative;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  @media (min-width: 1024px) {
+    flex-direction: row;
+    justify-content: start;
+    align-items: start;
+    overflow: hidden;
+  }
+`;
+
+const BannerContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 20%;
+  border-radius: 50px;
+  position: relative;
+  background-color: ${({ theme }) => {
+    return theme.isDarkMode ? '#22333b' : '#E9ECEF';
+  }};
+  order: 1;
+
+  @media (min-width: 1024px) {
+    order: 0;
+    width: 30%;
+    height: 100%;
+    border-radius: 0 0 80% 0;
+  }
+`;
+const BannerImage = styled.img`
+  display: none;
+
+  @media (min-width: 1024px) {
+    display: flex;
+    width: 70%;
+    height: 70%;
+    object-fit: cover;
+    position: absolute;
+    top: 75%;
+    left: 55%;
+    transform: translate(-50%, -50%);
+    filter: drop-shadow(0 20px 10px rgba(0, 0, 0, 0.2));
+  }
+`;
+
+const FormHolderContainer = styled.div`
+  display: flex;
+  width: 70%;
+  height: 100%;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  order: 0;
 `;
 
 // it's under ThemeProvider so theme props works. It's also defined in @emotion.d.ts
 const RouterText = styled(NavLink)`
   font-size: 1rem;
   color: ${({ theme }) => {
-    return theme.isDarkMode ? '#DEE2E6' : '#333333'
+    return theme.isDarkMode ? '#DEE2E6' : '#333333';
   }};
   text-align: right;
   padding: 0;
   margin: 0;
   text-decoration: none;
-  
+
   &:hover {
     text-decoration: underline;
   }
 `;
 
-export { Container, RouterText };
+export { Container, RouterText, BannerContainer, FormHolderContainer, BannerImage };
