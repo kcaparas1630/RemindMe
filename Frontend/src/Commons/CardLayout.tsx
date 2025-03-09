bc78ab525627cdc4e550180cc134e0da0aa1d3d2
Kent Caparas <kcaparas1630@gmail.com>
2025-03-08 21:56:22 -0800

    added rightItem as I was using a button for other cards, maintaining reusability

--- Frontend/src/Commons/CardLayout.tsx
+++ Frontend/src/Commons/CardLayout.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from "react";
-import { CardLayoutContainer, CardLayoutHeader, CardLayoutTitle, CardLayoutTitleText, CardLayoutDateText, CardLayoutContent } from "./StyledCommons/StyledCard";
+import { CardLayoutContainer, CardLayoutHeader, CardLayoutTitle, CardLayoutTitleText, CardLayoutRightItem, CardLayoutContent } from "./StyledCommons/StyledCard";
 import isDarkMode from "../Interface/General/isDarkMode";
 
 interface CardLayoutProps extends isDarkMode {
     title: string;
     children: React.ReactNode;
+    rightItem: React.ReactNode;
 }
 
-const CardLayout: FC<CardLayoutProps> = ({ title, children, isDarkMode }) => {
+const CardLayout: FC<CardLayoutProps> = ({ title, children, isDarkMode, rightItem }) => {
     return (
         <CardLayoutContainer isDarkMode={isDarkMode}>
             <CardLayoutHeader isDarkMode={isDarkMode}>
@@ -15,7 +16,7 @@ const CardLayout: FC<CardLayoutProps> = ({ title, children, isDarkMode }) => {
                     <CardLayoutTitleText>
                         {title}
                     </CardLayoutTitleText>
-                    <CardLayoutDateText>{new Date().toLocaleDateString()}</CardLayoutDateText>
+                    <CardLayoutRightItem>{rightItem}</CardLayoutRightItem>
                 </CardLayoutTitle>
             </CardLayoutHeader>
             <CardLayoutContent>
