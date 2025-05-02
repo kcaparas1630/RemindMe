f00cbf6dbd618700c0d8086a9ced98e7e3e63ee8
kcaparas1630 <kcaparas1630@gmail.com>
2025-05-02 12:22:38 -0700

    added more props to allow editing and use taskFormSection component. Allow DRY use

--- Frontend/src/Interface/DashboardProps.ts
+++ Frontend/src/Interface/DashboardProps.ts
@@ -1,9 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
 import GeneralProps from "./General/GeneralProps";
-
+import TaskInterface from "./TaskInterface";
 interface DashboardProps extends GeneralProps {
     userName: string;
     queryClient: QueryClient;
+    isEditing?: boolean;
+    onCancel?: () => void;
+    task?: TaskInterface;
 };
 
 export default DashboardProps;
